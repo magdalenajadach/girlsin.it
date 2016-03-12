d3c43b6cb2483e71b779d613433b9b8b13580976
Magdalena Jadach <magdalena.jadach@gmail.com>
2016-03-12 18:08:54 +0000

    Checked idea with adding 'on'.click

--- js/test.js
+++ js/test.js
@@ -5,20 +5,16 @@ $(document).ready(function() { /*I know that you told "forget about what we said
 //That was also a part of resolving problem with ReferenceError with body. More about it in point 3. */
 
 //1. I created new element ==> div
-
 var newDiv = $('<div></div');
 
 //2. I have selected <div> and added class to it:
-
 newDiv.attr('class', 'personal-info-container');
 
 //3. I appended <div> to a <body> tag
-
 /* newDiv.append(body); //=> since Friday night I had ReferenceError: "body is not defined"
 On Saturday I realised that maybe I dont't need to append div to a body. Maybe... ;) */
 
 //4. Added h1 element + added some text <p> to it:
-
 var newHeader = $('<h1></h1>');
 newHeader.appendTo(newDiv);
 newHeader.text('I am Magda');
@@ -37,12 +33,12 @@ newLink.appendTo(newDiv);
 //7. Time for img!
 newImage = $('<img></img>');
 $("img").attr({
-        "src": "https://avatars0.githubusercontent.com/u/8046557", //I am not sure when I should use double quotes and when single
-        "alt": "This is me!!!"
+    "src": "https://avatars0.githubusercontent.com/u/8046557", //I am not sure when I should use double quotes and when single
+    "alt": "This is me!!!"
     });
 newImage.appendTo(newDiv);
 
-("body").append( newDiv, newHeader, newParagraph ); //I don't know how this to the body.
+("body").append( newDiv, newHeader, newParagraph ); //I don't know how attach this to the body.
 
 });
  
