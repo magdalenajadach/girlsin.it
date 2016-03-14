2084196973631cf3c0d6f29b04d23329624a6da0
Magdalena Jadach <magdalena.jadach@gmail.com>
2016-03-14 19:31:32 +0000

    Task resolved

--- js/test.js
+++ js/test.js
@@ -5,14 +5,12 @@ $(document).ready(function() { /*I know that you told "forget about what we said
 //That was also a part of resolving problem with ReferenceError with body. More about it in point 3. */
 
 //1. I created new element ==> div
+var body = $('body');	
 var newDiv = $('<div></div');
 
 //2. I have selected <div> and added class to it:
 newDiv.attr('class', 'personal-info-container');
 
-//3. I appended <div> to a <body> tag
-/* newDiv.append(body); //=> since Friday night I had ReferenceError: "body is not defined"
-On Saturday I realised that maybe I dont't need to append div to a body. Maybe... ;) */
 
 //4. Added h1 element + added some text <p> to it:
 var newHeader = $('<h1></h1>');
@@ -20,25 +18,25 @@ newHeader.appendTo(newDiv);
 newHeader.text('I am Magda');
 
 //5. Added another one <p> + added some text to it:
-newParagraph = $('<p></p>')
+var newParagraph = $('<p></p>')
 newParagraph.text('I am a woman and I code');
-newParagraph.appendTo(newHeader);
-
+newParagraph.appendTo(newDiv);
+			
 //6. Let's play with href!
-newLink = $('<a></a>'); 
-$('a').attr('href', 'https://www.github.com/magdalenajadach'); 
+var newLink = $('<a></a>'); 
 newLink.text('This is my github profile');
+newLink.attr('href', 'https://www.github.com/magdalenajadach');
 newLink.appendTo(newDiv);
 
 //7. Time for img!
-newImage = $('<img></img>');
-$("img").attr({
+var newImage = $('<img></img>');
+newImage.appendTo(newDiv);
+newImage.attr({
     "src": "https://avatars0.githubusercontent.com/u/8046557", //I am not sure when I should use double quotes and when single
     "alt": "This is me!!!"
     });
-newImage.appendTo(newDiv);
 
-("body").append( newDiv, newHeader, newParagraph ); //I don't know how attach this to the body.
+newDiv.appendTo(body);
 
 });
  
