03c34994cc82c5e50370996e00c4491df87b1747
Magdalena Jadach <magdalena.jadach@gmail.com>
2016-05-07 21:03:57 +0100

    Added description in README, added style file + some style, changed app and controller name

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
-var magdaApp = angular.module('magdaApp', []);
+var ItsGirlsApp = angular.module('ItsGirlsApp', []);
 
-magdaApp.controller('MagdaController', function($scope, $rootScope, GuardianService){
-	$scope.name = 'Magda';
+ItsGirlsApp.controller('ItsGirlsController', function($scope, $rootScope, GuardianService){
 	GuardianService.getPublicFeed().then(function(response){
 		console.log(response);
 		$scope.feed = response.data;
@@ -9,7 +8,7 @@ magdaApp.controller('MagdaController', function($scope, $rootScope, GuardianServ
 		console.log('No data!');
 	});
 });
-magdaApp.factory('GuardianService', ['$http', function($http){
+ItsGirlsApp.factory('GuardianService', ['$http', function($http){
 	var service = {
 		getPublicFeed: function(){
 			var url = 'http://content.guardianapis.com/search?page-size=50&order-by=newest&q=women%20and%20tech&api-key=27f66131-9496-4cf7-b259-f054e97beedc';
