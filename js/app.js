f5d5fdb34d5679f55829d7c3d11d43080c7c8bd1
Magdalena Jadach <magdalena.jadach@gmail.com>
2016-05-08 12:44:59 +0100

    Added colors for web, another factory for api NYT

--- js/app.js
+++ js/app.js
@@ -17,3 +17,21 @@ ItsGirlsApp.factory('GuardianService', ['$http', function($http){
 	};
 	return service;
 }]); 
+
+ItsGirlsApp.controller('ItsGirlsControllerTest', function($scope, $rootScope, NewYorksTimesService){
+	NewYorksTimesService.getPublicFeed().then(function(result){
+		console.log(result);
+		$scope.feed = result.data;
+	}, function(result){
+		console.log('No data!');
+	});
+});	
+ItsGirlsApp.factory('NewYorksTimesService', ['$http', function($http){
+	var service = {
+		getPublicFeed: function(){
+			var url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=5da3b2f28d4f421da53c9edab39514b4';
+			return $http.get(url); 
+		}
+	};
+	return service;
+}]); 
